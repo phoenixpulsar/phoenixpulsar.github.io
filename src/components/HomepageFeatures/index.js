7aa95f6c6d2ad0eb44e11e4c1f1090ba3ad0b314
Phoenix Pulsar <phoenixpulsar@protonmail.com>
2024-12-24 13:58:45 -0600

    Change text in features back

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -9,7 +9,7 @@ const FeatureList = [
     href: "/blog/welcome",
     src: "",
     description:
-      "Howdy! Welcome to my notes on Teeeeee. In this space, I will document my learning journey into the technologies I am particularly interested in. This serves as a personal repository, a place to bookmark concepts, put ideas into my own words, and create cheat sheets for quick reference. Hopefully, this might also help someone else who shares an interest in these technologies. We’ll be diving into topics like the NEAR blockchain and developer productivity, which are currently at the top of the list. But I’ll definitely stay curious and wander down interesting tangents as they come up.Follow along as I embrace confusion, stumble through the process, and learn while building some cool stuff!",
+      "Howdy! Welcome to my notes on technology. In this space, I will document my learning journey into the technologies I am particularly interested in. This serves as a personal repository, a place to bookmark concepts, put ideas into my own words, and create cheat sheets for quick reference. Hopefully, this might also help someone else who shares an interest in these technologies. We’ll be diving into topics like the NEAR blockchain and developer productivity, which are currently at the top of the list. But I’ll definitely stay curious and wander down interesting tangents as they come up.Follow along as I embrace confusion, stumble through the process, and learn while building some cool stuff!",
   },
   {
     title: "Ordo One Encryption App",
